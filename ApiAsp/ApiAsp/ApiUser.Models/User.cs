7cda9c1d5452ecd8074c37d38a277717b35acea7
CRM202A <mdevoldere@users.noreply.github.com>
2023-05-05 11:54:00 +0200

    Api User (Model User)

---
+++ ApiAsp/ApiAsp/ApiUser.Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiUser.Models
+{
+    [Table("Users")]
+    public class User : Model
+    {
+        // [Column("user_name")]
+        [Required(ErrorMessage = "")]
+        [MaxLength(16, ErrorMessage = "")]
+        [StringLength(16)]
+        public string? UserName { get; set; }
+
+        public string? Password { get; set; }
+
+    }
+}
