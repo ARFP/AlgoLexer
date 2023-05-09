83c59a1e1c406b72ba97471ef0e8700eed744cad
CRM202A <mdevoldere@users.noreply.github.com>
2023-05-09 16:53:15 +0200

    User API Migration and Password BCRYPT

--- ApiAsp/ApiAsp/ApiUser.Models/User.cs
+++ ApiAsp/ApiAsp/ApiUser.Models/User.cs
@@ -4,15 +4,17 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace ApiUser.Models
 {
     [Table("Users")]
-    public class User : Model
+    public class User : Model 
     {
         // [Column("user_name")]
-        [Required(ErrorMessage = "")]
-        [MaxLength(16, ErrorMessage = "")]
-        [StringLength(16)]
+        [Required(ErrorMessage = "Le nom d'utilisateur est requis")]
+        // [MaxLength(16, ErrorMessage = "")]
+        [StringLength(maximumLength:16, MinimumLength = 2)]
+        [RegularExpression(@"^[a-zA-Z]+(?:\-[a-zA-Z]+)?$")]
         public string? UserName { get; set; }
-
+        
+        [Required(ErrorMessage = "Le mot de passe est obligatoire")]
+        [RegularExpression(@"^[a-zA-Z0-9]{8,}$")]
         public string? Password { get; set; }
-
     }
 }
---
+++ ApiAsp/ApiAsp/ApiUser/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using ApiUser.Db;
+using ApiUser.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiUser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+
+        public LoginController()
+        {
+            _context = new UserDbContext();
+        }
+
+        [HttpPost]
+        public User? Login(User userToLogin)
+        {
+            return _context.Users.FirstOrDefault(
+                u => u.UserName == userToLogin.UserName && 
+                u.Password == userToLogin.Password
+            );
+        }
+    }
+}
